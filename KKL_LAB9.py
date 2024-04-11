0caa7707e415a5fce0e693d1027144dd106473a8
kadenlendick <kadenlendick@gmail.com>
2024-04-11 12:38:47 -0400

    New

--- KKL_LAB9.py
+++ KKL_LAB9.py
@@ -1,2 +1,10 @@
 def encode(password):
-    
+    encoded_password = ""
+    for char in password:
+        if char in ["7", "8", "9"]:
+            encoded_char = str(int(char) + 3)[1]
+            encoded_password += encoded_char
+        else:
+            encoded_char = int(char) + 3
+            encoded_password += str(encoded_char)
+    return encoded_password
